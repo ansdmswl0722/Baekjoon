9843d1d5269c68aebb69999fa8089c946b71221c
문은지 <85018126+ansdmswl0722@users.noreply.github.com>
2022-12-02 19:02:50 +0900

    Update 오늘 날짜.java

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -1,6 +1,7 @@
 import java.time.*;
 public class Main{
     public static void main(String[] args){
+        // now() : 현재 시스템 시간을 yyyy-mm-dd 형태로 생성한다.
         LocalDate date = LocalDate.now();
         System.out.print(date);
     }
