47002cdda53b4d3f48c3b2b6e9d32d70c74760cc
문은지 <85018126+ansdmswl0722@users.noreply.github.com>
2022-12-06 16:08:06 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        while(s.hasNextInt()){System.out.println(s.nextInt()+s.nextInt());}
+    }
+}
