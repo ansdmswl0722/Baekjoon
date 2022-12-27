2cb370a40f24c8b625c7d33892087b4ec97ad5dc
문은지 <85018126+ansdmswl0722@users.noreply.github.com>
2022-12-27 18:24:46 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,12 +1,16 @@
 import java.util.*;
 import java.io.*;
+import java.util.stream.IntStream;
 public class Main{
     public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<Integer> submit = new ArrayList<Integer>();
-		for(int i=0;i<28;i++) submit.add(Integer.parseInt(br.readLine()));
-		for(int i=1;i<=30;i++){
-			 if(!submit.contains(i)) {System.out.println(i);}
-		}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean submit[] = new boolean[31]; int num =0;
+        for(int i=0;i<28;i++) { 
+        	num = Integer.parseInt(br.readLine());
+        	submit[num] = true;
+        }
+        for(int i=1;i<submit.length;i++) {
+        	if(!submit[i]) System.out.println(i);
+        }
   }
 }
