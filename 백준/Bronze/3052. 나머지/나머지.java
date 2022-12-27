caf496c0df6a1716438f1fceeb0b8b62e818f141
문은지 <85018126+ansdmswl0722@users.noreply.github.com>
2022-12-27 18:57:10 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -3,10 +3,10 @@ import java.io.*;
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int arr[] = new int[10],num=0; HashSet<Integer> set = new HashSet<Integer>();
+        int num=0; HashSet<Integer> set = new HashSet<Integer>();
         for(int i=0;i<10;i++){
-            arr[i] = Integer.parseInt(br.readLine())%42;
-            set.add(arr[i]);    
+            num = Integer.parseInt(br.readLine())%42;
+            set.add(num);    
         }
         System.out.println(set.size());
         
