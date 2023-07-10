b123b4a1121645e8c8395ba43c06aa835afe283c
문은지 <85018126+ansdmswl0722@users.noreply.github.com>
2023-07-10 18:10:04 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -4,14 +4,12 @@ stack=[]
 for i in range(n):
     stack.append(int(input()))
 
-coin = 0
 cnt = 0
 for i in range(n):
     coin = stack.pop()
     if coin <= k:
         cnt += k//coin
         k=k%coin
-        if k:
-            continue
-        else: break     
+        if not k:
+           break 
 print(cnt)   
