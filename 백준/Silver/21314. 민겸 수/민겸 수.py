e158a6b09647998aa6f0c8da5eeff4bf640fe2db
문은지 <85018126+ansdmswl0722@users.noreply.github.com>
2023-07-12 21:20:05 +0900

    [Silver II] Title: 민겸 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/21314. 민겸 수/민겸 수.py
+++ 백준/Silver/21314. 민겸 수/민겸 수.py
@@ -1,29 +1,22 @@
 n = input()
 
-min="0"
-max="0"
-for i in range(len(n)):
-    if n[i]=='K':
-       min += "5"
-    else:
-        if min[-1]=="1":
-            min += "0" 
-        elif i >0 and n[i-1]=="M":
-            min += "0"      
-        else:
-            min += "1"
+min=""
+max=""
 cnt=0 
 for i in range(len(n)):
-    if n[i] == 'K' and i==0:
-        max+='5'
-    elif n[i] =='K' and n[i-1]=='M':
-        max += "5"+"0"*cnt
-        cnt=0 
-    elif n[i] =='K' and n[i-1]=='K': 
-        max += "5"     
-    elif n[i] == 'M':
-        cnt+=1  
-    if i ==len(n)-1:
-        max+= "1"*cnt
+    if n[i] == 'M':
+        cnt+=1 
+        if i ==len(n)-1:
+            max+= "1"*cnt
+            min+= "1"+"0"*(cnt-1)
+    else:
+        if cnt > 0:
+            max+= "5"+"0"*cnt
+            min+= "1"+"0"*(cnt-1)+"5"
+            cnt=0
+        else:
+            max+= "5"
+            min+= "5"
+    
 print(int(max))
-print(int(min))          
+print(int(min))
