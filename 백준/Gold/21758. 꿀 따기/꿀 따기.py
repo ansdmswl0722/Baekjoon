d479ff176864689e1bcdd5b5615c6e4524f6d183
문은지 <85018126+ansdmswl0722@users.noreply.github.com>
2023-07-19 10:35:06 +0900

    [Gold V] Title: 꿀 따기, Time: 224 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.py
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.py
@@ -3,19 +3,20 @@ n = int(sys.stdin.readline())
 place = list(map(int,sys.stdin.readline().split()))
 honey = sum(place)
 sum1=0
-for i in range(1,n-1):
+b=place[0]
+for i in range(1,n):
     a = honey - place[0]- place[i]
-    b = sum(place[i+1:])
-    sum1 = max(sum1,a+b)
+    b += place[i]
+    sum1 = max(sum1,a+honey-b)
 
 for i in range(1,n-1):
     a = honey - place[0]-place[-1]+ place[i]
     sum1=max(sum1,a)
     
 place.reverse()
-for i in range(1,n-1):
+b=place[0]
+for i in range(1,n):
     a = honey - place[0]- place[i]
-    b = sum(place[i+1:])
-    sum1 = max(sum1,a+b)   
-
+    b += place[i]
+    sum1 = max(sum1,a+honey-b)
 print(sum1)
