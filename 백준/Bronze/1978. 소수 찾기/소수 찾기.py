d6c195f6ce7db73bef9070fa568d17813553bcdd
문은지 <85018126+ansdmswl0722@users.noreply.github.com>
2023-08-21 11:47:35 +0900

    [Bronze II] Title: 소수 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,15 +1,15 @@
 import sys
 n = int(input())
 nums = list(map(int,input().split()))
-
 cnt=0
+sieve = [1]*1001
+sieve[0]=0
+sieve[1]=0
+for k in range(2,len(sieve)):
+    if sieve[k]==0: continue
+    for m in range(2*k,len(sieve),k):
+        sieve[m]=0
+
 for i in nums:
-    answer=True
-    if i==1:
-        continue
-    for j in range(2,i):
-        if i%j==0:
-            answer=False
-    if answer:    
-        cnt+=1
+   if sieve[i]==1: cnt+=1
 print(cnt)
